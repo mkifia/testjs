7708f3419972e731a42fe8c9ae5b35323d9f10be
Moustakime KIFIA <mkifia@icloud.com>
2016-01-07 22:24:51 +0100

    test with chai assertion library

--- test/percentageSpec.js
+++ test/percentageSpec.js
@@ -1,5 +1,8 @@
 var Percentage = require('../lib/Percentage');
-var assert = require('assert');
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+
+require('chai').should();
 
 describe('Percentage', function () {
 	
@@ -8,17 +11,17 @@ describe('Percentage', function () {
 		// 
 		it('give an evolution in percentage', function() {
 
-			assert.equal(Percentage.evolution(100, 200), 100);
-			assert.equal(Percentage.evolution(100, 150), 50);
-			assert.equal(Percentage.evolution(100, 50), -50);
+			expect(Percentage.evolution(100, 200)).to.be.equal(100);
+			expect(Percentage.evolution(100, 150)).to.be.equal(50);
+			expect(Percentage.evolution(100, 50)).to.be.equal(-50);
 		});
 
 		it('handle 0 in evolution', function() {
-			assert.equal(Percentage.evolution(0, 100), Infinity);
+			expect(Percentage.evolution(0, 100)).to.be.equal(Infinity);
 		});
 
 		it('Should round off two-digit after the comma', function() {
-			assert.equal(Percentage.evolution(30, 100), 233.33)
+			expect(Percentage.evolution(30, 100)).to.be.equal(233.33);
 		})
 	});
 })
