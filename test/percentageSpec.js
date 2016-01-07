1f4011f6c999704ff85e87c239aa55d20ac16b9a
Moustakime KIFIA <mkifia@icloud.com>
2016-01-07 21:03:06 +0100

    add test for a round number

--- test/percentageSpec.js
+++ test/percentageSpec.js
@@ -11,6 +11,14 @@ describe('Percentage', function () {
 			assert.equal(Percentage.evolution(100, 200), 100);
 			assert.equal(Percentage.evolution(100, 150), 50);
 			assert.equal(Percentage.evolution(100, 50), -50);
+		});
+
+		it('handle 0 in evolution', function() {
+			assert.equal(Percentage.evolution(0, 100), Infinity);
+		});
+
+		it('Should round off two-digit after the comma', function() {
+			assert.equal(Percentage.evolution(30, 100), 233.33)
 		})
-	})
+	});
 })
