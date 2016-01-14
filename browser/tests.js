7055189459bd7d57f14473c18df55270e36ee37e
Moustakime KIFIA <mkifia@icloud.com>
2016-01-14 11:59:22 +0100

    test on browser

---
+++ browser/tests.js
@@ -0,0 +1,12 @@
+var expect = chai.expect;
+
+describe('Twitter', function() {
+	it('Is instaciable', function() {
+		var t = new Twitter();
+		expect(t.test).to.be.equal(2);
+	});
+});
+
+it('Multiply', function () {
+	
+});
